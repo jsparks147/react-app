8d478a7b38f175300853ec29f5618a0d8beb33f6
Joe Sparks <jsparks@mathworks.com>
2022-07-20 15:48:18 -0400

    add build and merge workflow

--- src/App.js
+++ src/App.js
@@ -17,9 +17,7 @@ function App() {
                 >
                     Learn React
                 </a>
-                <p>
-                    Joe Was here
-                </p>
+                <p>Joe Was here</p>
             </header>
         </div>
     );
